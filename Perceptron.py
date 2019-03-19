0849ed5b85139258666f22c121eba77fb367bb9c
themaflyman <xuziwei100@gmail.com>
2019-03-19 22:26:35 +0800

    Add test class method

--- Perceptron.py
+++ Perceptron.py
@@ -6,6 +6,10 @@ import numpy as np
 from sklearn.datasets import load_iris
 import matplotlib.pyplot as plt
 from Optimizer import Optimizer
+from Dataset import IrisDataset
+
+
+IRIS_DATASET = IrisDataset()
 
 
 class Perceptron:
@@ -71,31 +75,34 @@ class Perceptron:
     def score(self):
         pass
 
+    @classmethod
+    def test(cls):
+        df = pd.DataFrame(IRIS_DATASET.data, columns=IRIS_DATASET.feature_names)
+        df['label'] = IRIS_DATASET.target
+        df.columns = ['sepal length', 'sepal width', 'petal length', 'petal width', 'label']
+        df.label.value_counts()
+        data = np.array(df.iloc[:100, [0, 1, -1]])
+        x_train, y_train = data[:, :-1], data[:, -1]
+        y_train = np.array([1 if i == 1 else -1 for i in y_train])
+
+        # training
+        perceptron = cls()
+        perceptron.fit(x_train, y_train)
+        print(perceptron.w, perceptron.b)
+
+        # testing
+        x_points = np.linspace(4, 7, 10)
+        y_ = -(perceptron.w[0] * x_points + perceptron.b) / perceptron.w[1]
+        plt.plot(x_points, y_)
+
+        plt.plot(data[:50, 0], data[:50, 1], 'bo', color='blue', label='0')
+        plt.plot(data[50:100, 0], data[50:100, 1], 'bo', color='orange', label='1')
+        plt.xlabel('sepal length')
+        plt.ylabel('sepal width')
+        plt.legend()
+        plt.show()
+        cls.fit()
+
 
 if __name__ == "__main__":
-    # Prepare for training set
-    iris = load_iris()
-    df = pd.DataFrame(iris.data, columns=iris.feature_names)
-    df['label'] = iris.target
-    df.columns = ['sepal length', 'sepal width', 'petal length', 'petal width', 'label']
-    df.label.value_counts()
-    data = np.array(df.iloc[:100, [0, 1, -1]])
-    x_train, y_train = data[:, :-1], data[:, -1]
-    y_train = np.array([1 if i == 1 else -1 for i in y_train])
-
-    # training
-    perceptron = Perceptron()
-    perceptron.fit(x_train, y_train)
-    print(perceptron.w, perceptron.b)
-
-    # testing
-    x_points = np.linspace(4, 7, 10)
-    y_ = -(perceptron.w[0]*x_points + perceptron.b)/perceptron.w[1]
-    plt.plot(x_points, y_)
-
-    plt.plot(data[:50, 0], data[:50, 1], 'bo', color='blue', label='0')
-    plt.plot(data[50:100, 0], data[50:100, 1], 'bo', color='orange', label='1')
-    plt.xlabel('sepal length')
-    plt.ylabel('sepal width')
-    plt.legend()
-    plt.show()
+    Perceptron.test()
