6838cc725ce3df4d510fc2b4dd93b15a11b1cbc5
themaflyman <xuziwei100@gmail.com>
2019-06-19 10:50:27 +0800

    add comments for indicator_function

--- util.py
+++ util.py
@@ -2,8 +2,13 @@
 # -*- encoding: utf8 -*-
 
 
-# TODO: comments
 def indicator_function(*value_sets, cond):
+    """
+    A function indicates if all value sets meet the condition
+    :param value_sets: a list of value sets to evaluate
+    :param cond: condition
+    :return: 1 or 0
+    """
     for value_set in value_sets:
         if not cond(*value_set):
             return 1
