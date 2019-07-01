4e003d0f7a110d7bc22d3e77f826afde179652dc
themaflyman <xuziwei100@gmail.com>
2019-07-01 11:12:45 +0800

    Add convergence term and comments

--- hmm.py
+++ hmm.py
@@ -4,19 +4,77 @@
 import numpy as np
 
 
+# TODO: A general logger for all Estimator.
 class HiddenMarkovModel:
-    """
+    """Implementations of Hidden Markov Model.
 
     Parameters
     ----------
-    initial_state_probability_matrix : numpy.ndarray
-    transition_probability_matrix : numpy.ndarray
-    observation_probability_matrix : numpy.ndarray
+    initial_state_prob : numpy.ndarray
+        Initial state occupation distribution.
+
+    transition_prob : numpy.ndarray
+        Matrix of transition probabilities between states.
+
+    emission_prob : numpy.ndarray
+        Matrix of probabilities of an observation for a given state emission.
+
     observation_set : tuple
-        a tuple of observations, observations are numbered by their order
+        a tuple of observations, observations are numbered by their order.
 
     state_set : tuple
-        a tuple of states, states are numbered by their order
+        a tuple of states, states are numbered by their order.
+
+    max_iteration: int, optional
+        Maximum number of iterations to perform.
+
+    criterion : float, optional
+        Convergence threshold.
+
+    Attributes
+    ----------
+    state_prob : numpy.ndarray
+        State occupation distribution.
+
+    transition_prob : numpy.ndarray
+        Matrix of transition probabilities between states.
+
+    emission_prob : numpy.ndarray
+        Matrix of probabilities of an observation for a given state emission.
+
+    state_set_num : int
+        Number of states in the model.
+
+    observation_set_num : int
+        Number of the symbols of observations in the model.
+
+    observation_len : int
+        Length of the given observations sequence.
+
+    forward_prob : numpy.ndarray
+        Probabilities of seeing the given observations sequence, using the
+        Forward Algorithm.
+
+    backward_prob : numpy.ndarray
+        Probabilities of seeing the given observations sequence, using the
+        Backward Algorithm.
+
+    Examples
+    --------
+    >>> initial_state_prob = np.array([.2, .4, .4])
+    >>> transition_prob = np.array([[.5, .2, .3],
+    ...                               [.3, .5, .2],
+    ...                                [.2, .3, .5]])
+    >>> emission_prob = np.array([[.5, .5],
+    ...                              [.4, .6],
+    ...                              [.7, .3]])
+    >>> hmm = HiddenMarkovModel(initial_state_prob=initial_state_prob,
+    ...                         transition_prob=transition_prob,
+    ...                         emission_prob=emission_prob,
+    ...                         observation_set=('红', '白'),
+    ...                         state_set=(1, 2, 3))
+    >>> hmm.fit(['红', '白', '红'])
+    >>> print(hmm.decode(['红', '白', '红']))
 
     Notes
     -----
@@ -25,6 +83,9 @@ class HiddenMarkovModel:
     ----------
     """
     def __init__(self,
+                 initial_state_prob,
+                 transition_prob,
+                 emission_prob,
                  observation_set,
                  state_set,
                  **kwargs):
@@ -34,40 +95,59 @@ class HiddenMarkovModel:
         self.state_set = state_set
         self.state_set_num = len(self.state_set)
 
-        self.max_iteration = kwargs.get("max_iteration", 100)
-        self.state_prob = kwargs.get("initial_state_prob",
-                                     np.ones(self.state_set_num)
-                                     / self.state_set_num)
-        self.transition_prob = kwargs.get("transition_prob",
-                                          np.ones((self.state_set_num,
-                                                   self.state_set_num)))
-        self.emission_prob = kwargs.get("emission_prob",
-                                        np.ones((self.state_set_num,
-                                                 self.observation_set_num)))
+        self.state_prob = initial_state_prob,
+        self.transition_prob = transition_prob
+        self.emission_prob = emission_prob
 
-        self.observations = None
+        self.max_iteration = kwargs.get("max_iteration", 50)
+        self.criterion = kwargs.get('criterion', 1e-3)
+
         self.observation_len = None
 
         self.forward_prob = None
         self.backward_prob = None
 
-    def _compute_forward_prob(self):
+    def _compute_forward_prob(self, observations):
+        """Compute the forward probabilities of seeing the given observations
+           sequence under the model and its current parameters.
+
+        Parameters
+        ----------
+        observations: array-like
+
+        Returns
+        -------
+        forward_prob: array-like
+            Forward probabilities of seeing the given observations sequence.
+        """
         forward_prob = np.zeros((self.observation_len, self.state_set_num))
 
         forward_prob[0] = \
             self.state_prob \
-            * self.emission_prob[:, self.observations[0]]
+            * self.emission_prob[:, observations[0]]
 
         for t in range(1, self.observation_len):
             for i in range(self.state_set_num):
                 forward_prob[t][i] = sum(
                     [forward_prob[t-1][j] * self.transition_prob[j][i]
                      for j in range(self.state_set_num)]) \
-                    * self.emission_prob[i][self.observations[t]]
+                    * self.emission_prob[i][observations[t]]
 
         return forward_prob
 
-    def _compute_backward_prob(self):
+    def _compute_backward_prob(self, observations):
+        """Compute the backward probabilities of seeing the given observations
+           sequence under the model and its current parameters.
+
+        Parameters
+        ----------
+        observations: array-like
+
+        Returns
+        -------
+        backward_prob: array-like
+            Backward probabilities of seeing the given observations sequence.
+        """
         backward_prob = np.zeros((self.observation_len, self.state_set_num))
 
         backward_prob[self.observation_len-1] = np.ones(self.state_set_num)
@@ -76,57 +156,134 @@ class HiddenMarkovModel:
             for i in range(self.state_set_num):
                 backward_prob[t][i] = sum(
                     self.transition_prob[i][j]
-                    * self.emission_prob[j][self.observations[t+1]]
+                    * self.emission_prob[j][observations[t+1]]
                     * backward_prob[t+1][j]
                     for j in range(self.state_set_num))
 
         return backward_prob
 
-    def _compute_o(self, t, i, j):
+    def _compute_expected_state_transition_count(self, observations, t, i, j):
+        """Compute the expected state transition count under the model and its
+           current parameters.
+
+        Parameters
+        ----------
+        observations : array-like
+        t : int
+            index of time t
+        i : int
+            index of state i
+        j : int
+            index of state j
+
+        Returns
+        -------
+        expected_state_occupancy_count : float
+            Probability of being in state i at time t and
+            state j at time t+1.
+        """
         return self.forward_prob[t][i] \
                * self.transition_prob[i][j] \
-               * self.emission_prob[j][self.observations[t+1]] \
+               * self.emission_prob[j][observations[t+1]] \
                * self.backward_prob[t+1][j] \
                / sum(sum(self.forward_prob[t][i]
                          * self.transition_prob[i][j]
-                         * self.emission_prob[j][self.observations[t+1]]
+                         * self.emission_prob[j][observations[t+1]]
                          * self.backward_prob[t+1][j]
                          for j in range(self.state_set_num))
                      for i in range(self.state_set_num))
 
-    def _compute_t(self, t, i):
+    def _compute_expected_state_occupancy_count(self, t, i):
+        """Compute the expected state occupancy count under the model and its
+           current parameters.
+
+        Parameters
+        ----------
+        t : int
+            index of time t
+        i : int
+            index of state i
+
+        Returns
+        -------
+        expected_state_transition_count : float
+            Probability of being in state i at time t.
+        """
         return self.forward_prob[t][i] * self.backward_prob[t][i] \
                / sum(self.forward_prob[t][j] * self.backward_prob[t][j]
                      for j in range(self.state_set_num))
 
-    def learn(self, observations):
-        self.observation_len = len(observations)
-        self.observations = [self.observation_set.index(observation)
-                             for observation in observations]
+    def _compute_log_likelihood(self, observations):
+        pass
 
-        # TODO: Add convergence term
+    def fit(self, observations):
+        """Fit the model to the given observations sequence.
+
+        Parameters
+        ----------
+        observations : array-like
+        """
+        self.observation_len = len(observations)
+        observations = [self.observation_set.index(observation)
+                        for observation in observations]
+
         for _ in range(self.max_iteration):
-            self.forward_prob = self._compute_forward_prob()
-            self.backward_prob = self._compute_backward_prob()
+            self.forward_prob = self._compute_forward_prob(observations)
+            self.backward_prob = self._compute_backward_prob(observations)
+
+            updated_state_prob = np.zeros(self.state_set_num)
+            updated_transition_prob = np.zeros((self.state_set_num,
+                                                self.state_set_num))
+            updated_emission_prob = np.zeros((self.state_set_num,
+                                              self.observation_set_num))
 
             for i in range(self.state_set_num):
                 for j in range(self.state_set_num):
-                    self.transition_prob[i][j] = \
-                        sum(self._compute_o(t, i, j)
+                    updated_transition_prob[i][j] = \
+                        sum(self._compute_expected_state_transition_count(
+                            observations, t, i, j)
                             for t in range(self.observation_len-1)) \
-                        / sum(self._compute_t(t, i)
+                        / sum(self._compute_expected_state_occupancy_count(t, i)
                               for t in range(self.observation_set_num-1))
 
             for j in range(self.state_set_num):
                 for k in range(self.observation_set_num):
-                    self.emission_prob[j][k] = \
-                        sum(self._compute_t(t, j)
+                    updated_emission_prob[j][k] = \
+                        sum(self._compute_expected_state_occupancy_count(t, j)
                             for t in range(self.observation_len)
-                            if self.observations[t] == k) \
-                        / sum(self._compute_t(t, j)
+                            if observations[t] == k) \
+                        / sum(self._compute_expected_state_occupancy_count(t, j)
                               for t in range(self.observation_len))
 
+            for i in range(self.state_set_num):
+                updated_state_prob[i] = \
+                    self._compute_expected_state_occupancy_count(0, i)
+
+            if np.max(abs(updated_state_prob-self.state_prob)) \
+               + np.max(abs(updated_transition_prob-self.transition_prob)) \
+               + np.max(updated_emission_prob-self.emission_prob) \
+               < self.criterion * 3.0:
+                break
+
+            self.state_prob = updated_state_prob
+            self.transition_prob = updated_transition_prob
+            self.emission_prob = updated_emission_prob
+
     def decode(self, observations, decoder='viterbi'):
+        """Decode the model with the given observations sequence and specified
+           decoder.
+
+        Parameters
+        ----------
+        observations : array-like
+        decoder : string, optional
+           Decoder the model uses to decode the observations sequence.
+
+        Returns
+        -------
+        states : array-like
+            A most likely state sequence of the given observatons sequence.
+        """
         observations = [self.observation_set.index(observation)
                         for observation in observations]
 
@@ -157,8 +314,8 @@ class HiddenMarkovModel:
                   transition_prob=transition_prob,
                   emission_prob=emission_prob,
                   observation_set=('红', '白'), state_set=(1, 2, 3),
-                  max_iteration=1)
-        hmm.learn(['红', '白', '红'])
+                  max_iteration=50)
+        hmm.fit(['红', '白', '红'])
         print(hmm.forward_prob)
         print(hmm.backward_prob)
         print(hmm.transition_prob)
@@ -177,6 +334,18 @@ def viterbi_backtrack_best_path(observation_len,
 
 
 def decode_by_viterbi(hmm, observations):
+    """Decode a Hidden Markov Model using Viterbi Algorithm.
+
+    Parameters
+    ----------
+    hmm: HiddenMarkovModel
+    observations: array-like
+
+    Returns
+    -------
+    states : array-like
+        A most likely state sequence of the given observatons sequence.
+    """
     observation_len = len(observations)
     path_prob = np.zeros((len(observations), hmm.state_set_num))
     path_pointer = np.zeros((len(observations), hmm.state_set_num))
